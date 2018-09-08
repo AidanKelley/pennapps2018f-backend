e75bc4582ef2392e2ffb13c25e07ba92f800a139
Aidan Kelley <aidanmkelley@gmail.com>
2018-09-08 10:48:16 -0400

    Added sample config file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    aws: {
+        region: "your AWS region",
+        endpoint: "your endpoint",
+        accessKeyId: "your access key id",
+        secretAccessKey: "your access key"
+    },
+    secret: "secret to be used for the JWT signing"
+};
